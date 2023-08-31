6e34eaaeb6ca991c862a1a8a6bf6c97d4a34de72
CC11001100 <CC11001100@qq.com>
2023-08-31 23:47:17 +0800

    feat: 锁的基本功能测试

---
+++ basic.go
@@ -0,0 +1,26 @@
+package storage_lock_test_helper
+
+import (
+	"context"
+	storage_lock_factory "github.com/storage-lock/go-storage-lock-factory"
+	"github.com/storage-lock/go-utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// BasicTest 锁的基本功能测试
+func BasicTest[Connection any](t *testing.T, factory *storage_lock_factory.StorageLockFactory[Connection]) {
+
+	lockId := utils.RandomID()
+	ownerId := utils.RandomID()
+
+	lock, err := factory.CreateLock(lockId)
+	assert.Nil(t, err)
+
+	err = lock.Lock(context.Background(), ownerId)
+	assert.Nil(t, err)
+
+	err = lock.UnLock(context.Background(), ownerId)
+	assert.Nil(t, err)
+
+}
